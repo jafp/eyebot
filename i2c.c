edd785a8556f4e484e279d8011461568469b3444
Jacob Pedersen <jacob@itbuster.dk>
2013-10-09 22:23:03 +0200

    Fixed minor functional errors in the i2c module.

--- i2c.c
+++ i2c.c
@@ -32,76 +32,67 @@ int i2c_bus_close()
 	// Noop
 }
 
-
 int i2c_cmd_read(int addr, char cmd, char buffer[], int length)
 {
-	int ret_val;
-	struct i2c_rdwr_ioctl_data packets;
+    int ret_val;
+    struct i2c_rdwr_ioctl_data packets;
     struct i2c_msg messages[2];
 
     // Prepare command message
-    messages[0].addr = 0x28;
+    messages[0].addr = addr;
     messages[0].flags = 0;
     messages[0].len = sizeof(cmd);
     messages[0].buf = &cmd;
-
+    
     if (length > 0)
     {
-    	// Receive message 
-		messages[1].addr = 0x28;
-	    messages[1].flags = I2C_M_RD;
-	    messages[1].len = length;
-	    messages[1].buf = buffer;    	
+        // Receive message 
+        messages[1].addr = addr;
+        messages[1].flags = I2C_M_RD;
+        messages[1].len = length;
+        messages[1].buf = buffer;       
     }
 
     packets.msgs = messages;
-    packets.nmsgs = 1 + length;
-
-    // Packet and messages prepared. Tell driver to transmit
-    // and receive from the specific slave.
+    packets.nmsgs = 2;
 
     ret_val = ioctl(dev_fd, I2C_RDWR, &packets);
     if (ret_val < 0)
     {
-    	perror("ioctl I2C_RDWR");
+        perror("ioctl I2C_RDWR");
     }
 
-    return ret_val;
+    return ret_val < 0 ? ret_val : length;
 }
 
 int i2c_cmd_write(int addr, char cmd, char buffer[], int length)
 {
-	int ret_val;
-	struct i2c_rdwr_ioctl_data packets;
+    int ret_val;
+    struct i2c_rdwr_ioctl_data packets;
     struct i2c_msg messages[2];
 
-    // Prepare command message
-    messages[0].addr = 0x28;
-    messages[0].flags = 0;
-    messages[0].len = sizeof(cmd);
-    messages[0].buf = &cmd;
+    char * buf = malloc(length + 1);
+    buf[0] = cmd;
 
-    if (length > 0)
-    {
-    	// Receive message 
-		messages[1].addr = 0x28;
-	    messages[1].flags = 0;
-	    messages[1].len = length;
-	    messages[1].buf = buffer;    	
-    }
+    memcpy(buf + 1, buffer, length);
+
+    // Prepare command message
+    messages[0].addr = addr;
+    messages[0].flags = 0;
+    messages[0].len = length + 1;
+    messages[0].buf = buf;
 
     packets.msgs = messages;
-    packets.nmsgs = 1 + length;
-
-    // Packet and messages prepared. Tell driver to transmit
-    // and receive from the specific slave.
+    packets.nmsgs = 1;
 
     ret_val = ioctl(dev_fd, I2C_RDWR, &packets);
     if (ret_val < 0)
     {
-    	perror("ioctl I2C_RDWR");
+        perror("ioctl I2C_RDWR");
     }
 
-    return ret_val;
+    free(buf);
+
+    return ret_val < 0 ? ret_val : length;
 }
 
